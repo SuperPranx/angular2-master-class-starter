28f4f77d31b123b4c56db0aa53fd22c4c4c05bd6
Ivan Milanov <ivan.milanov@netcetera.com>
2017-02-07 11:34:49 +0100

    Removed obsolete lines

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -9,22 +9,6 @@ import { CONTACT_DATA } from './data/contact-data';
 })
 
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
-  contact:Contact = {
-    id: 6,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  }
   contacts:Contact[] = CONTACT_DATA;
   
   trackById(index:number, contact:Contact) {
